c4cd3fe5e3d80cccdb394184277be8e78f7d1a35
Marko Berdovic <berdovicm@gmail.com>
2025-05-07 23:14:24 +0200

    Definisana klasa i instancirani objekti po uzoru na skicu

---
+++ prodavnica/example.js
@@ -0,0 +1,12 @@
+class Artikal {
+    constructor(broj, naziv, cena, dostupan){
+        this.broj=broj
+        this.naziv=naziv
+        this.cena=cena
+        this.dostupan=dostupan
+    }
+}
+
+const monitor = new Artikal(1, "monitor", 165, true)
+const TV = new Artikal(2, "TV",650, false)
+const mis = new Artikal(3,"mis",20, true)
